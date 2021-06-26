737a568734ca06c9932c4c1f2539e6611389a8a0
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-06-26 11:52:44 +0500

    Dynamic column add
    Adding columns dynamically.

--- resources/views/front_page/front_page.blade.php
+++ resources/views/front_page/front_page.blade.php
@@ -57,5 +57,22 @@
 				</section>
 			</form>
 		</div>
+		<script type="text/javascript">
+			$('.addRow').on('click',function(){
+					addRow();
+			});
+			function addRow()
+			{
+					var tr='<tr>'+
+						'<td><input type="text" name="product_name[]" class="form-control" required=""></td>'+
+						'<td><input type="text" name="brand[]" class="form-control"></td>'+
+						'<td><input type="text" name="quantity[]" class="form-control quantity" required=""></td>'+
+						'<td><input type="text" name="budget[]" class="form-control budget"></td>'+
+						'<td><input type="text" name="amount[]" class="form-control amount"></td>'+
+						'<td><a href="#" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove"></i></a></td>'+
+					'</tr>';
+					$('tbody').append(tr);
+			};
+		</script>
 	</body>
 </html>
